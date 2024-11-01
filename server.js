fb1e63b57e0f519e6bfb5191f99634b6ff51c12a
greybo <sergeybotl@gmail.com>
2024-11-01 21:32:39 +0200

    change comment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const admin = require('firebase-admin');
 
-// Ініціалізація Firebase Admin SDK
+// Initializing the Firebase Admin SDK
 const serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -16,8 +16,9 @@ const PORT = process.env.PORT || 3035;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Middleware для перевірки токена
+// Middleware for token validation
 const authenticateToken = async (req, res, next) => {
+  console.log('Check token');
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
@@ -32,11 +33,12 @@ const authenticateToken = async (req, res, next) => {
   }
 };
 
-// Захищений маршрут для збереження даних
+// Secure route for data storage
 app.post('/users', authenticateToken, async (req, res) => {
   const { id, dataObject } = req.body;
 
   try {
+    console.log('Try users end point');
     await admin.database().ref(`users/${id}`).set(dataObject);
     res.status(200).send({ message: 'Data saved successfully 2', path: `users/${id}` });
   } catch (error) {
@@ -45,14 +47,14 @@ app.post('/users', authenticateToken, async (req, res) => {
   }
 });
 
-// Маршрут для аутентифікації (тільки для демонстрації, не використовуйте на production)
+// Authentication route (for demonstration only, do not use in production)
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const userRecord = await admin.auth().getUserByEmail(email);
-    // Nota bene: В реальному додатку ніколи не перевіряйте паролі на сервері
-    // Це лише для демонстрації
+// Nota bene: In a real application, never check passwords on the server
+// This is just for demonstration purposes
     const token = await admin.auth().createCustomToken(userRecord.uid);
     res.status(200).send({ message: 'User logged in', token });
   } catch (error) {
